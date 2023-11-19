6c3a59b22e7480aeb21384966827bcd325b7ff37
Alireza <alirezanege.1404@gmail.com>
2023-11-19 17:06:00 +0330

    FEAT: Collision between units

---
+++ collision/collision.go
@@ -0,0 +1,33 @@
+package collision
+
+import (
+	"gogl/unit"
+	"math"
+
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+const (
+	// Minimum distance for collision is unit size
+	UnitSize = 0.01
+	// Angle between distance vector and z-axis
+	theta = 40
+)
+
+func UnitsCollide(unitA, unitB *unit.Unit) bool {
+
+	// Calculate the distance between unit centers
+	distanceVector := mgl32.Vec3{unitA.X, unitA.Y, unitA.Z}.Sub(mgl32.Vec3{unitB.X, unitB.Y, unitB.Z})
+	distance := distanceVector.Len()
+	if distance < UnitSize {
+		// Define the z-axis direction
+		zAxis := mgl32.Vec3{0, 0, 1}
+
+		// Calculate the angle between the distance vector and the z-axis
+		angleRadians := math.Acos(float64(distanceVector.Normalize().Dot(zAxis.Normalize())))
+		angle := mgl32.RadToDeg(float32(angleRadians))
+		return angle < theta
+	}
+
+	return false
+}
